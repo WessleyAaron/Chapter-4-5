2343ebfc023659e673dd913bb5a560b1be3881a5
WessleyAaron <37811241+WessleyAaron@users.noreply.github.com>
2018-04-16 12:24:00 -0500

    update triangle

--- Triangle/Triangle/Source.cpp
+++ Triangle/Triangle/Source.cpp
@@ -9,88 +9,109 @@
 int main()
 {
 	double sideA, sideB, sideC, base, height, hypotenuse, area;
-	std::string userInputSide, userInputTriangleShape;
+	std::string userInputSide, userInputTriangleShape, userRepeatChoice;
 
-	std::cout << "What triangle do you want to solve for? right, isosceles, or equilateral?" << std::endl;
-	std::cin >> userInputTriangleShape;
-
-	if (userInputTriangleShape == "right" || userInputTriangleShape == "Right")
+	do
 	{
-		std::cout << "Which side of the right triangle would you like to solve? A, B, or C?" << std::endl;
-		std::cin >> userInputSide;
+		std::cout << "What triangle do you want to solve for? right, isosceles, or equilateral?" << std::endl;
+		std::cin >> userInputTriangleShape;
 
-		if (userInputSide == "a" || userInputSide == "A")
+		if (userInputTriangleShape == "right" || userInputTriangleShape == "Right")
 		{
-			std::cout << "You chose side " << userInputSide << ". What is the distance for side B?" << std::endl;
-			std::cin >> sideB;
+			std::cout << "Which side of the right triangle would you like to solve? A, B, or C?" << std::endl;
+			std::cin >> userInputSide;
 
-			std::cout << "What is the distance for side C?" << std::endl;
-			std::cin >> sideC;
+			if (userInputSide == "a" || userInputSide == "A")
+			{
+				std::cout << "You chose side " << userInputSide << ". What is the distance for side B?" << std::endl;
+				std::cin >> sideB;
 
-			sideA = sqrt((sideC * sideC) - (sideB * sideB));
-			area = (sideA * sideB) / 2;
+				std::cout << "What is the distance for side C?" << std::endl;
+				std::cin >> sideC;
 
-			std::cout << "The distance for side A is " << sideA << " and the area is " << area << std::endl;
+				sideA = sqrt((sideC * sideC) - (sideB * sideB));
+				area = (sideA * sideB) / 2;
+
+				std::cout << "The distance for side A is " << sideA << " and the area is " << area << std::endl;
+			}
+
+			else if (userInputSide == "b" || userInputSide == "B")
+			{
+				std::cout << "You chose side " << userInputSide << ". What is the distance for side A?" << std::endl;
+				std::cin >> sideA;
+
+				std::cout << "What is the distance for side C?" << std::endl;
+				std::cin >> sideC;
+
+				sideB = sqrt((sideC * sideC) - (sideA * sideA));
+				area = (sideA * sideB) / 2;
+
+				std::cout << "The distance for side B is " << sideB << " and the area is " << area << std::endl;
+			}
+
+			else if (userInputSide == "C" || userInputSide == "c")
+			{
+				std::cout << "You chose side " << userInputSide << ". What is the distance for side A?" << std::endl;
+				std::cin >> sideA;
+
+				std::cout << "What is the distance for side B?" << std::endl;
+				std::cin >> sideB;
+
+				sideC = sqrt((sideA * sideA) + (sideB * sideB));
+				area = (sideA * sideB) / 2;
+
+				std::cout << "The distance for side C is " << sideC << " and the area is " << area << std::endl;
+			}
 		}
 
-		else if (userInputSide == "b" || userInputSide == "B")
+		else if (userInputTriangleShape == "isosceles" || userInputTriangleShape == "Isosceles")
 		{
-			std::cout << "You chose side " << userInputSide << ". What is the distance for side A?" << std::endl;
-			std::cin >> sideA;
+			std::cout << "What is the distance of side A (the base)?" << std::endl;
+			std::cin >> base;
 
-			std::cout << "What is the distance for side C?" << std::endl;
-			std::cin >> sideC;
+			std::cout << "What is the distance of side B (the hypotenuse)?" << std::endl;
+			std::cin >> hypotenuse;
 
-			sideB = sqrt((sideC * sideC) - (sideA * sideA));
-			area = (sideA * sideB) / 2;
+			height = sqrt((hypotenuse * hypotenuse) - ((base * base) * .25));
+			area = (base * height) / 2;
+			//hypotenuse = side b
+			//base = side a
 
-			std::cout << "The distance for side B is " << sideB << " and the area is " << area << std::endl;
+			std::cout << "The height of the isosceles triangle is " << height << " and the area is " << area << std::endl;
+
 		}
 
-		else if (userInputSide == "C" || userInputSide == "c")
+		else if (userInputTriangleShape == "equilateral" || userInputTriangleShape == "Equilateral")
 		{
-			std::cout << "You chose side " << userInputSide << ". What is the distance for side A?" << std::endl;
-			std::cin >> sideA;
+			std::cout << "What is the distance of the sides of the triangle?" << std::endl;
+			std::cin >> base;
 
-			std::cout << "What is the distance for side B?" << std::endl;
-			std::cin >> sideB;
+			height = ((sqrt(3)) * base) / 2;
+			area = (base * height) * .5;
+			//hypotenuse = side a
+			//base = side b
 
-			sideC = sqrt((sideA * sideA) + (sideB * sideB));
-			area = (sideA * sideB) / 2;
-
-			std::cout << "The distance for side C is " << sideC << " and the area is " << area << std::endl;
+			std::cout << "The area of the triangle is " << area << " and the height is " << height << std::endl;
 		}
-	}
 
-	else if (userInputTriangleShape == "isosceles" || userInputTriangleShape == "Isosceles")
+		std::cout << "Would you like to solve another triangle?" << std::endl;
+		std::cin >> userRepeatChoice;
+	} 
+	
+	while (userRepeatChoice == "yes" || userRepeatChoice == "yes");
+
+	std::cout << "Goodbye" << std::endl;
+
+	/*if (userRepeatChoice == "yes" || userRepeatChoice == "Yes")
 	{
-		std::cout << "What is the distance of side A (the base)?" << std::endl;
-		std::cin >> base;
-
-		std::cout << "What is the distance of side B (the hypotenuse)?" << std::endl;
-		std::cin >> hypotenuse;
-
-		height = sqrt((hypotenuse * hypotenuse) - ((base * base) * .25));
-		area = (base * height) / 2;
-		//hypotenuse = side b
-		//base = side a
-
-		std::cout << "The height of the isosceles triangle is " << height << " and the area is " << area << std::endl;
-
+		main();
 	}
 
-	else if (userInputTriangleShape == "equilateral" || userInputTriangleShape == "Equilateral")
+	else
 	{
-		std::cout << "What is the distance of the sides of the triangle?" << std::endl;
-		std::cin >> base;
-		
-		area = (base * height) * .5; //math is wrong here
-		height = ((sqrt(3)) * base) / 2;
-		//hypotenuse = side a
-		//base = side b
+		std::cout << "Goodbye" << std::endl;
 
-		std::cout << "The area of the triangle is " << area << " and the height is " << height << std::endl;
-	}
+	}*/
 
 	system("pause");
 	return 0;
