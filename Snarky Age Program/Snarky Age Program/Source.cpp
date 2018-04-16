da0c4fb9b07782c73215ff996e4df00f5f8433ae
WessleyAaron <37811241+WessleyAaron@users.noreply.github.com>
2018-04-16 13:34:39 -0500

    update Snarky Age

--- Snarky Age Program/Snarky Age Program/Source.cpp
+++ Snarky Age Program/Snarky Age Program/Source.cpp
@@ -7,12 +7,12 @@
 
 int main()
 {
-	double userAge = 0;
+	double userAge;
 
 	std::cout << "Please enter your age" << std::endl;
 	std::cin >> userAge;
 
-	if (userAge = 5 || userAge == 6)
+	if (userAge == 5 || userAge == 6)
 	{
 		std::cout << "You're a little young to be using this" << std::endl;
 	}
@@ -22,7 +22,7 @@ int main()
 		std::cout << "You should be playing outside" << std::endl;
 	}
 
-	else if (userAge = 11 || userAge == 12 || userAge == 13 || userAge == 14 || userAge == 15)
+	else if (userAge == 11 || userAge == 12 || userAge == 13 || userAge == 14 || userAge == 15)
 	{
 		std::cout << "Do you have your homework done?" << std::endl;
 	}
